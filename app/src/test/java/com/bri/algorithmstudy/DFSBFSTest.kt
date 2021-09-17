bd98b876743611e8b084279c3654467fc9ee9818
정서윤 <seoyoon630@neobns.com>
2021-09-17 14:04:29 +0900

    ---

--- app/src/test/java/com/bri/algorithmstudy/DFSBFSTest.kt
+++ app/src/test/java/com/bri/algorithmstudy/DFSBFSTest.kt
@@ -8,7 +8,7 @@ class DFSBFSTest {
     fun main(){
 //        assertThat({DFSBFSAlg.factorial()}, 120.toDouble(), "factorial")
 //        assertThat({DFSBFSAlg._유클리드호제법()}, 6, "_유클리드호제법")
-        assertThat({DFSBFSAlg.dfs()}, Unit, "DFS")
-        assertThat({DFSBFSAlg.bfs()}, Unit, "BFS")
+//        assertThat({DFSBFSAlg.dfs()}, Unit, "DFS")
+//        assertThat({DFSBFSAlg.bfs()}, Unit, "BFS")
     }
 }
