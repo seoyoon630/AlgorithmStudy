513702bffb37b674f502d7a94d22998c52abd6c5
정서윤 <seoyoon630@neobns.com>
2021-10-14 10:20:58 +0900

    Dynamic 알고리즘 : 피보나치수열 문제풀이

---
+++ app/src/test/java/com/bri/algorithmstudy/DynamicTest.kt
@@ -0,0 +1,11 @@
+@file:Suppress("FunctionName", "NonAsciiCharacters")
+package com.bri.algorithmstudy
+
+import org.junit.Test
+
+class DynamicTest {
+    @Test
+    fun main(){
+        DynamicAlg._피보나치수열()
+    }
+}
