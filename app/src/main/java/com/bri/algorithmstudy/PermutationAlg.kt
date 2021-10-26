80123e294ca89dc8013ca77cdff515f7b05fe13e
정서윤 <seoyoon630@neobns.com>
2021-10-26 15:58:24 +0900

    순열 알고리즘 추가

--- app/src/main/java/com/bri/algorithmstudy/PermutationAlg.kt
+++ app/src/main/java/com/bri/algorithmstudy/PermutationAlg.kt
@@ -18,4 +18,9 @@ object PermutationAlg {
         1 -> listOf(temp + numsList)
         else -> numsList.flatMap { num -> _permutation(nums, temp + num, numsList - num) }
     }
+
+    fun <T> permutation(el: List<T>, fin: List<T> = listOf(), sub: List<T> = el ): List<List<T>> {
+        return if(sub.isEmpty()) listOf(fin)
+        else sub.flatMap { permutation(el, fin + it, sub - it) }
+    }
 }
