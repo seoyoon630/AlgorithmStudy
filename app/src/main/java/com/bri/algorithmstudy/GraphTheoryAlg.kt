f970a3ee63a1b21d2010c2f138a3994b492bd3e2
정서윤 <seoyoon630@neobns.com>
2021-11-03 17:13:37 +0900

    그래프 이론 : 크루스칼 알고리즘 예제

--- app/src/main/java/com/bri/algorithmstudy/GraphTheoryAlg.kt
+++ app/src/main/java/com/bri/algorithmstudy/GraphTheoryAlg.kt
@@ -9,43 +9,65 @@ object GraphTheoryAlg {
      * - 합집합, 찹기 연산 지원
      * - 루트 노드에 즉시 접근 불가능
      * 합치기연산 : 큰 노드의 부모를 작은 노드로 설정하며 합쳐나감.
+     * 2. 사이클 판별
+     * - 무방향 그래프 내에서 사이클을 판별하기 위해 서로소 집합 알고리즘을 사용
+     * (방향 그래프에서는 DFS를 통해 사이클 여부 판별)
+     * - 각 간선을 하나씩 확인하며 두 노드의 루트 노드를 확인
+     * - 루트 노드가 서로 다르면 두 노드에 대하여 합집합 연산을 수행
+     * - 루트 노드가 같다면 사이클이 발생
+     * 3. 최소신장트리
+     * - 그래프에서 모든 노드를 포함하면서 사이클이 존재하지 않는 부분 그래프
+     * -> 최소한의 비용으로 구성되는 신장 트리를 찾는 문제
+     * = N개의 도시가 존재하는 상황에서 두 도시 사이에 다리를 놓아 전체 도시가 연결될 수 있도록 도로를 설치하는 문제
+     * - 간선의 개수는 (N - 1)개
+     * 4. 크루스칼 알고리즘
+     * - 대표적인 최소 신장 트리 알고리즘
+     * - 그리디 알고리즘
+     * - 동작 과정
+     *  1. 간선 데이터를 비용에 따라 오름차순으로 정렬
+     *  2. 간선을 하나씩 확인하며 현재의 간선이 사이클을 발생시키는지 확인
+     *   2-1. 사이클이 발생하지 않으면 최소 신장 트리에 포함
+     *   2-2. 사이클이 발생하면 최소 신장 트리에서 제외
+     *  3. 모든 간선에 대해 2번의 과정을 반복
      */
-
-    private fun findParent(parent: IntArray, x: Int): Int {
-        return if (parent[x] != x) findParent(parent, parent[x])
-        else x
-    }
+//
+//    private fun findParent(parent: IntArray, x: Int): Int {
+//        return if (parent[x] != x) findParent(parent, parent[x])
+//        else x
+//    }
+//
+//    private fun unionParent(parent: IntArray, a: Int, b: Int) {
+//        val aParent = findParent(parent, a)
+//        val bParent = findParent(parent, b)
+//        if (aParent < bParent) parent[b] = a
+//        else parent[a] = b
+//    }
+//
+//    fun _서로소집합자료구조(n: Int, pairs: List<IntArray>) {
+//        val parent = IntArray(n + 1) { it }
+//        pairs.forEach { unionParent(parent, it[0], it[1]) }
+//
+//        // 집합 찾기
+//        val groups = HashMap<Int, ArrayList<Int>>()
+//        for (i in 1..n) {
+//            val root = findParent(parent, i)
+//            groups[root]?.add(i) ?: run { groups[root] = arrayListOf(i) }
+//        }
+//        groups.forEach { (root, set) -> println("$root {${set.joinToString()}}") }
+//    }
 
     private fun advancedFindParent(parent: IntArray, x: Int): Int {
-        if (parent[x] != x) parent[x] = findParent(parent, parent[x])
+        if (parent[x] != x) {
+            parent[x] = advancedFindParent(parent, parent[x])
+        }
         return parent[x]
-    }
-
-    private fun unionParent(parent: IntArray, a: Int, b: Int) {
-        val aParent = findParent(parent, a)
-        val bParent = findParent(parent, b)
-        if (aParent < bParent) parent[b] = aParent
-        else parent[a] = bParent
     }
 
     private fun advancedUnionParent(parent: IntArray, a: Int, b: Int) {
         val aParent = advancedFindParent(parent, a)
         val bParent = advancedFindParent(parent, b)
-        if (aParent < bParent) parent[b] = aParent
-        else parent[a] = bParent
-    }
-
-    fun _서로소집합자료구조(n: Int, pairs: List<IntArray>) {
-        val parent = IntArray(n + 1) { it }
-        pairs.forEach { unionParent(parent, it[0], it[1]) }
-
-        // 집합 찾기
-        val groups = HashMap<Int, ArrayList<Int>>()
-        for (i in 1..n) {
-            val root = findParent(parent, i)
-            groups[root]?.add(i) ?: run { groups[root] = arrayListOf(i) }
-        }
-        groups.forEach { (root, set) -> println("$root {${set.joinToString()}}") }
+        if (aParent < bParent) parent[bParent] = aParent
+        else parent[aParent] = bParent
     }
 
     fun _개선된서로소집합자료구조(n: Int, pairs: List<IntArray>) {
@@ -58,5 +80,57 @@ object GraphTheoryAlg {
             groups[parent[i]]?.add(i) ?: run { groups[parent[i]] = arrayListOf(i) }
         }
         groups.forEach { (root, set) -> println("$root {${set.joinToString()}}") }
+    }
+
+    fun _서로소집합을활용한사이클판별(n: Int, pairs: List<IntArray>): Boolean {
+        val parent = IntArray(n + 1) { it }
+        pairs.forEach {
+            if (advancedFindParent(parent, it[0]) == advancedFindParent(parent, it[1])) {
+                return true
+            } else advancedUnionParent(parent, it[0], it[1])
+        }
+
+        return false
+    }
+
+    fun _크루스칼알고리즘(
+        n: Int = 7,
+        pairs: List<IntArray> = listOf(
+            intArrayOf(1, 2, 29),
+            intArrayOf(1, 5, 75),
+            intArrayOf(2, 3, 35),
+            intArrayOf(2, 6, 34),
+            intArrayOf(3, 4, 7),
+            intArrayOf(4, 6, 23),
+            intArrayOf(4, 7, 13),
+            intArrayOf(5, 6, 53),
+            intArrayOf(6, 7, 25)
+        )
+    ): Int {
+        var answer = 0
+        val result = ArrayList<IntArray?>()
+        // 오름차순으로 정렬
+        val sorted = pairs.sortedWith { o1, o2 -> o1[2] - o2[2] }
+
+        // 사이클이 발생하는지 확인
+        val parent = IntArray(n + 1) { it }
+        sorted.forEach {
+            val a = it[0]
+            val b = it[1]
+            val distance = it[2]
+            // 사이클 발생 X
+            if (advancedFindParent(parent, a) != advancedFindParent(parent, b)) {
+                println("$a ---- $b 성공")
+                result.add(intArrayOf(a, b))
+                advancedUnionParent(parent, a, b)
+                answer += distance
+            } else {
+                println("$a ---- $b 실패")
+            }
+        }
+
+        println("연결된 간선 ${result.size}개")
+        println(result.joinToString(" | ") { it?.joinToString() ?: "" })
+        return answer
     }
 }
--- app/src/test/java/com/bri/algorithmstudy/GraphTheoryTest.kt
+++ app/src/test/java/com/bri/algorithmstudy/GraphTheoryTest.kt
@@ -17,5 +17,12 @@ class GraphTheoryTest {
 //        val v = List(n - 1) { intArrayOf(it + 1, it + 2) }
 //        assertThat({ GraphTheoryAlg._서로소집합자료구조(n, v) }, Unit, "서로소집합자료구조")
 //        assertThat({ GraphTheoryAlg._개선된서로소집합자료구조(n, v) }, Unit, "개선된서로소집합자료구조")
+//        assertThat({
+//            GraphTheoryAlg._서로소집합을활용한사이클판별(
+//                3,
+//                listOf(intArrayOf(1, 2), intArrayOf(2, 3), intArrayOf(1, 3))
+//            )
+//        }, true, "서로소집합을활용한사이클판별")
+//        assertThat({ GraphTheoryAlg._크루스칼알고리즘() }, 159, "크루스칼알고리즘")
     }
 }
